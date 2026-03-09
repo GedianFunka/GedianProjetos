03b1e15fa15c591e157647b2beed77084c626447
gedianfunka <gediangabrielf@gmail.com>
2026-03-09 08:35:08 -0300

    Update PessoaMain.java

--- src/Pessoa/PessoaMain.java
+++ src/Pessoa/PessoaMain.java
@@ -1,12 +1,35 @@
 package Pessoa;
 
+import java.util.Scanner;
+
 public class PessoaMain {
 
 	public static void main(String[] args) {
 
+	/*Pessoa p1 = new Pessoa("Natalino", 39, "753.746.413-72");
+
+	System.out.println(p1.getNome() + "\n" + p1.getCpf() + "\n" +  p1.getIdade());
 	
+	p1.setIdade(99);
 	
+	System.out.println(p1.getIdade());
+	*/
 	
+	Scanner s = new Scanner(System.in);
+	
+	System.out.println("Digite seu nome: ");
+	String nome = s.nextLine();
+	
+	System.out.println("Digite sua idade: ");
+	int idade = s.nextInt();
+	
+	s.nextLine();
+	
+	System.out.println("Digite seu cpf: ");
+	String cpf = s.nextLine();
+	
+	s.close();	
+		
 	}
 
 }
