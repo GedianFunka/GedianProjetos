bef125a2b98d0911854e8f2ed5a1d41e75d51de4
gedianfunka <gediangabrielf@gmail.com>
2026-03-09 08:15:31 -0300

    Encapsulamento

---
+++ src/AULA01/Pessoa.java
@@ -0,0 +1,50 @@
+package AULA01;
+
+public class Pessoa {
+
+	        //atributos
+	        private String nome;
+	        private int idade;
+	        private String cpf;
+	        
+	        //construtor completo
+	        public Pessoa(String nome, int idade, String cpf) {
+	            this.nome = nome;
+	            this.idade = idade;
+	            this.cpf = cpf;
+	            
+	        }
+	        
+	        
+	        //criando metodos de acesso getters e setters
+	        //getters -> chama atributo .get
+	        //setters -> modifica o atributo .set
+
+			public String getNome() {
+				return nome;
+			}
+
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+
+			public int getIdade() {
+				return idade;
+			}
+
+			public void setIdade(int idade) {
+				this.idade = idade;
+			}
+
+			public String getCpf() {
+				return cpf;
+			}
+
+			public void setCpf(String cpf) {
+				this.cpf = cpf;
+			}
+	
+	        
+	        
+	    }
+	
