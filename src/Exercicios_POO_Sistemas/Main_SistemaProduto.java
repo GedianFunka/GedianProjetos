14c5b5baf5324388183294b1428143710e1c1c9d
Gedian_G_Funka <gediangabrielf@gmail.com>
2026-03-12 19:54:56 -0300

    ok

--- src/Exercicios_POO_Sistemas/Main_SistemaProduto.java
+++ src/Exercicios_POO_Sistemas/Main_SistemaProduto.java
@@ -1,9 +1,37 @@
 package Exercicios_POO_Sistemas;
 
+import java.util.*;
+
 public class Main_SistemaProduto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		
+		ArrayList <String> produtos = new ArrayList <>();
+		
+		int opcao;
+		
+		do {
+			System.out.println("===Cadastro de produtos");
+			System.out.println("1. Adicionar produtos");
+			System.out.println("2. Listar produtos");
+			System.out.println("3. Contar produtos");
+			System.out.println("0. SAIR");
+			
+			opcao = s.nextInt();
+			s.nextLine();
+			
+			switch (opcao) {
+			
+			case 1 :
+				
+			}
+			
+		}
+		while (opcao != 0);
+		
+		s.close();
 
 	}
 
--- src/Exercicios_POO_Sistemas/SistemaProduto.java
+++ src/Exercicios_POO_Sistemas/SistemaProduto.java
@@ -2,4 +2,43 @@ package Exercicios_POO_Sistemas;
 
 public class SistemaProduto {
 
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public SistemaProduto(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public void ExibirDados() {
+		System.out.println("O produto" + nome + " está com o preço de " + preco + " e sua quantidade em estoque é: " + quantidade + "\n");
+		
+	}
 }
+
